f84465edf6fadf6566ccc1f77f8fffbfe395276c
Nicholas Garrett <garrett.nick.96@gmail.com>
2021-04-17 22:56:29 -0500

    Splits notification settings into components

---
+++ components/PushNotifications.ts
@@ -0,0 +1,43 @@
+import Constants from "expo-constants";
+import * as Permissions from "expo-permissions";
+import * as Notifications from "expo-notifications";
+
+export interface DailyTriggerInput {
+  hour: number;
+  minute: number;
+  repeats: true;
+}
+
+export const scheduleNotifications = (
+  hasPushNotifsEnabled: boolean,
+  timeFromState: [number, number]
+) => {
+  const trigger: DailyTriggerInput = {
+    hour: timeFromState[0],
+    minute: timeFromState[1],
+    repeats: true,
+  };
+  const schedulingOptions = {
+    content: {
+      title: "Habitify Wellness",
+      body: "You can now submit your progress for today",
+      sound: true,
+      priority: Notifications.AndroidNotificationPriority.HIGH,
+    },
+    trigger,
+  };
+  Notifications.cancelAllScheduledNotificationsAsync();
+  if (hasPushNotifsEnabled) {
+    Notifications.scheduleNotificationAsync(schedulingOptions);
+  }
+};
+
+export const handleNotification = () => {
+  console.warn("Notification received.");
+};
+
+export const askNotification = async () => {
+  const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+  if (Constants.isDevice && status === "granted")
+    console.log("Notification permissions granted.");
+};
---
+++ components/PushNotificationsCell.tsx
@@ -0,0 +1,108 @@
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View, Switch } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import * as Notifications from "expo-notifications";
+
+import { updateToggledSetting } from "../state/user_settings/actions";
+import { useAppDispatch, useAppSelector } from "../state/user_settings/hooks";
+import { isTrackingNothing } from "../state/user_settings/helpers";
+import * as PushNotifications from "./PushNotifications";
+import theme from "./theme";
+
+interface SettingProps {
+  settingName: string;
+  iconName: string;
+  description: string;
+  actionName: string;
+}
+
+const PushNotificationsCell: React.FC<SettingProps> = (props): JSX.Element => {
+  const { settingName, iconName, description, actionName } = props;
+  const dispatch = useAppDispatch();
+
+  var hasPushNotifsEnabled: boolean = useAppSelector((state) => {
+    return state.settings.userSettings.pushNotificationsEnabled;
+  });
+
+  const timeFromState: [number, number] = useAppSelector((state) => {
+    return state.settings.userSettings.hourAndMinute;
+  });
+
+  const toggleSwitch = () => {
+    dispatch(updateToggledSetting(actionName, !hasPushNotifsEnabled));
+    hasPushNotifsEnabled = !hasPushNotifsEnabled;
+    PushNotifications.scheduleNotifications(
+      hasPushNotifsEnabled,
+      timeFromState
+    );
+  };
+
+  useEffect(() => {
+    PushNotifications.askNotification();
+    const listener = Notifications.addNotificationReceivedListener(
+      PushNotifications.handleNotification
+    );
+    return () => listener.remove();
+  }, []);
+
+  return (
+    <View style={styles.cell}>
+      <Ionicons
+        name={iconName}
+        color={isTrackingNothing() ? "grey" : theme.colors.border}
+        size={30}
+        style={{ marginLeft: 20 }}
+      />
+      <View>
+        <Text
+          style={[
+            styles.title,
+            isTrackingNothing() ? styles.disabled : styles.enabled,
+          ]}
+        >
+          {settingName}
+        </Text>
+        <Text style={{ fontSize: 12, marginLeft: 15, color: "grey" }}>
+          {description}
+        </Text>
+      </View>
+      <Switch
+        style={styles.switch}
+        trackColor={{
+          false: theme.colors.notification,
+          true: theme.colors.border,
+        }}
+        thumbColor={theme.colors.primary}
+        onValueChange={toggleSwitch}
+        value={hasPushNotifsEnabled}
+        disabled={isTrackingNothing()}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  cell: {
+    paddingTop: 10,
+    paddingBottom: 15,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 16,
+    marginLeft: 15,
+  },
+  switch: {
+    marginLeft: "auto",
+    marginRight: 20,
+    transform: [{ scaleX: 1.3 }, { scaleY: 1.3 }],
+  },
+  disabled: {
+    color: "grey",
+  },
+  enabled: {
+    color: "black",
+  },
+});
+
+export default PushNotificationsCell;
