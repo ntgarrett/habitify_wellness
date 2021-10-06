b2569570f4fb0043846249ea66c5a9bb5f0d0cfc
Nick Garrett <garrett.nick.96@gmail.com>
2021-10-06 13:55:26 -0700

    Removes an action

--- state/user_settings/actions.tsx
+++ state/user_settings/actions.tsx
@@ -3,14 +3,6 @@ export const updateToggledSetting = (actionName: string, value: boolean) => ({
   payload: value,
 });
 
-export const updateDaysExercisePerWeek = (
-  actionName: string,
-  value: number
-) => ({
-  type: actionName,
-  payload: value,
-});
-
 export const updateScheduleTime = (
   actionName: string,
   value: [number, number]
