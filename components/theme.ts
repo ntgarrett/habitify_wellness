7bf5ca343d76814e1b92e98016973ca2d3033513
Nick Garrett <garrett.nick.96@gmail.com>
2021-10-06 14:00:39 -0700

    Adjusts color theme

--- components/theme.ts
+++ components/theme.ts
@@ -2,11 +2,14 @@ const theme = {
   dark: false,
   colors: {
     primary: "#FFF",
-    background: "#FFF",
-    card: "#3D5A80",
+    background: "#EDEDE8",
+    card: "#155D27",
     text: "#FFF",
-    border: "#293241",
+    border: "#10451D",
     notification: "#808080",
+    data1: "#ff0000",
+    data2: "#ffff00",
+    data3: "#00ff00",
   },
 };
 
